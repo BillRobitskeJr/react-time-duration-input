a2388c8088330c0a61c89b52d907a64976b8ff0f
Bill Robitske <bill.robitske.jr@gmail.com>
2019-06-16 14:57:41 -0500

    feat(render): display value as string
    Basic display of value property as a string duration, where value is in terms of days

--- src/components/TimeDurationInput/index.jsx
+++ src/components/TimeDurationInput/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TimeDurationInput () {
+export default function TimeDurationInput ({ value }) {
+  const [ duration ] = useState(convertValueToDuration(value))
   return (
-    <input type='text' />
+    <input type='text' value={duration} data-testid='duration-input' />
   )
+}
+
+export function convertValueToDuration (value) {
+  if (!value) return '0d'
+  const days = Math.floor(value)
+  const hours = Math.floor(value * 24 % 24)
+  const minutes = Math.floor(value * 1440 % 60)
+  return [
+    days && `${days}d`,
+    hours && `${hours}h`,
+    minutes && `${minutes}m`
+  ].filter(x => !!x).join(' ')
 }
--- test/TimeDurationInput.test.js
+++ test/TimeDurationInput.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { render, cleanup } from '@testing-library/react'
+import 'jest-dom/extend-expect'
 
 import TimeDurationInput from '../src/components/TimeDurationInput'
 
@@ -8,5 +10,34 @@ describe('TimeDurationInput', () => {
     const div = document.createElement('div')
     ReactDOM.render(<TimeDurationInput />, div)
     ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('presents an input element', () => {
+    const { getByTestId } = render(<TimeDurationInput />)
+    const input = getByTestId('duration-input')
+    expect(input).toBeInTheDocument()
+    cleanup()
+  })
+
+  describe('render with value (basic)', () => {
+    afterEach(cleanup)
+
+    test('when value = 1.5, input shows "1d 12h"', () => {
+      const { getByTestId } = render(<TimeDurationInput value={1.5} />)
+      const input = getByTestId('duration-input')
+      expect(input).toHaveValue('1d 12h')
+    })
+
+    test('when value = 0, input shows "0d"', () => {
+      const { getByTestId } = render(<TimeDurationInput value={0} />)
+      const input = getByTestId('duration-input')
+      expect(input).toHaveValue('0d')
+    })
+
+    test('when value is not provided, input shows "0d"', () => {
+      const { getByTestId } = render(<TimeDurationInput />)
+      const input = getByTestId('duration-input')
+      expect(input).toHaveValue('0d')
+    })
   })
 })
