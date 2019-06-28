5fad34badcb66e629b963af07aff6acc525bde7e
Bill Robitske, Jr <bill.robitske.jr@gmail.com>
2019-06-28 12:46:11 -0500

    feat(scale): add scale property
    Add scale property for handling values at scales other than milliseconds

--- src/components/TimeDurationInput/index.js
+++ src/components/TimeDurationInput/index.js
@@ -1,18 +1,19 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-export function TimeDurationInput ({ value, onChange, className }) {
-  const [ duration, setDuration ] = useState(convertValueToDuration(value))
+export function TimeDurationInput ({ value, scale, onChange, className }) {
+  const [ duration, setDuration ] = useState(convertFromValue(value, scale))
+
   useEffect(() => {
-    const newDuration = convertValueToDuration(value)
+    const newDuration = convertFromValue(value, scale)
     if (newDuration !== duration) setDuration(newDuration)
-  }, [ value ])
+  }, [ value, scale ])
 
   const onInputChange = useCallback(({ target }) => {
     setDuration(target.value)
-    const newValue = convertDurationToValue(target.value)
+    const newValue = convertToValue(target.value, scale)
     if (!isNaN(newValue)) onChange(newValue)
-  }, [ onChange ])
+  }, [ onChange, scale ])
 
   return (
     <input type="text" className={className} value={duration} onChange={onInputChange} data-testid="duration-input" />
@@ -21,12 +22,30 @@ export function TimeDurationInput ({ value, onChange, className }) {
 
 TimeDurationInput.propTypes = {
   value: PropTypes.number,
+  scale: PropTypes.oneOf([ 'd', 'h', 'm', 's', 'ms' ]),
   onChange: PropTypes.func,
   className: PropTypes.string
 }
 
 TimeDurationInput.defaultProps = {
+  scale: 'ms',
   onChange: () => {}
+}
+
+export const SCALE_CONVERSIONS = {
+  ms: 1,
+  s: 1000,
+  m: 60000,
+  h: 3600000,
+  d: 86400000
+}
+
+export function convertValueFromScale (value, scale) {
+  return value * (SCALE_CONVERSIONS[scale] || 1)
+}
+
+export function convertValueToScale (value, scale) {
+  return value / (SCALE_CONVERSIONS[scale] || 1)
 }
 
 export function convertValueToDuration (value) {
@@ -50,5 +69,9 @@ export function convertDurationToValue (duration) {
   const [days, hours, minutes, seconds, milliseconds] = matches.slice(1).map(x => parseInt(x) || 0)
   return (((days * 24 + hours) * 60 + minutes) * 60 + seconds) * 1000 + milliseconds
 }
+
+export const convertFromValue = (value, scale) => convertValueToDuration(convertValueFromScale(value, scale))
+
+export const convertToValue = (duration, scale) => convertValueToScale(convertDurationToValue(duration), scale)
 
 export default TimeDurationInput
---
+++ src/components/TimeDurationInput/scale.test.js
@@ -0,0 +1,43 @@
+import { convertValueFromScale, convertValueToScale } from './index'
+
+const SCENARIOS = [
+  { value: 0, scales: { d: 0, h: 0, m: 0, s: 0, ms: 0 }}
+]
+
+describe('convertValueFromScale(value, scale)', () => {
+  describe('when scale is invalid', () => {
+    it('returns the passed value', () => {
+      const value = Math.random()
+      expect(convertValueFromScale(value, 'invalid')).toBeCloseTo(value)
+    })
+  })
+
+  SCENARIOS.forEach(({ value, scales }) => {
+    Object.keys(scales).forEach(scale => {
+      describe(`when value = ${scales[scale]} and scale = "${scale}"`, () => {
+        it(`returns ${value}`, () => {
+          expect(convertValueFromScale(scales[scale], scale)).toBeCloseTo(value)
+        })
+      })
+    })
+  })
+})
+
+describe('convertValueToScale(value, scale)', () => {
+  describe('when scale is invalid', () => {
+    it('returns the passed value', () => {
+      const value = Math.random()
+      expect(convertValueToScale(value, 'invalid')).toBeCloseTo(value)
+    })
+  })
+
+  SCENARIOS.forEach(({ value, scales }) => {
+    Object.keys(scales).forEach(scale => {
+      describe(`when value = ${value} and scale = "${scale}"`, () => {
+        it(`returns ${scales[scale]}`, () => {
+          expect(convertValueToScale(value, scale)).toBeCloseTo(scales[scale])
+        })
+      })
+    })
+  })
+})
